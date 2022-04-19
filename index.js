1a8a8b32cbf83eb5835482e421533a780608c5d2
belfortep <pbelforte2003@gmail.com>
2022-04-19 08:40:57 -0300

    commit

--- index.js
+++ index.js
@@ -20,12 +20,23 @@ client.on('messageCreate', (message) => {
 
     if (message.content.includes('!test')) {
         message.channel.send('testing');
+        message.delete()
     }
 
     if (message.content === '!paolo') {
         message.channel.send('https://github.com/belfortep');
     }
 
+    if (message.content === '!nice') {
+        const niceMessage = new Discord.MessageEmbed()
+            .setTitle('A nice message')
+            .setColor('RED')
+            .addField('Something', "Lorem")
+            .setAuthor('Paolo', 'https://avatars.githubusercontent.com/u/75280184?v=4')
+
+        message.channel.send({ embeds: [niceMessage] })
+
+    }
 
 })
 
